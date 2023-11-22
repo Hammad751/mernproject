771bb6c687325e273248761e9726b877ef59b28f
virtuByVirtual <hammadvtech@gamil.com>
2023-11-22 18:38:00 +0500

    connect to database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 import express from "express";
+import mongoose from "mongoose"
+import dotenv from  'dotenv'
+
+dotenv.config() 
 
 const app = express();
 const port = 8000;
 
+try {
+    mongoose.connect(process.env.MONGO)
+    console.log("database connected...");
+} 
+catch (error) {
+    console.log("connection error");
+}
 
 app.listen(port, () =>{
     console.log(`server is listening on port http://localhost:${port}`);
