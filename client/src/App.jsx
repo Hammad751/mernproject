c4b134ec434a9b1e217198eeedd0d250bc09ae33
virtuByVirtual <hammadvtech@gamil.com>
2023-11-22 16:33:16 +0500

    create routes and change title

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
+import SignIn from "./pages/SignIn.jsx"
+import SignUp from "./pages/SignUp.jsx"
+import Profile from "./pages/Profile.jsx"
 
+// everything is covered within browser router
 function App() {
   return (
     <div>
-      <h1 className='text-red-600 '>App </h1>
+      <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/profile' element={<Profile />}/>
+          <Route path='/signin' element={<SignIn />}/>
+          <Route path='/signup' element={<SignUp />}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
-
 export default App
