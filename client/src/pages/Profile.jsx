301ea7b0365cbf4e8f389efe1d193601bc4206b7
virtuByVirtual <hammadvtech@gamil.com>
2023-12-01 13:00:39 +0500

    complete profile UI page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,44 +1,57 @@
-import { current } from '@reduxjs/toolkit'
-import React from 'react'
+import { useSelector } from 'react-redux'
 
 export default function Profile() {
 
+  const { currentUser } = useSelector(state => state.user);
+  // handleChange
   return (
-    <div>
-      <input 
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <img 
+          src={currentUser.profilePicture} alt='profile'
+          className='h-24 w-24 self-center cursor-pointer rounded-full object-cover'
+        />
+        <input 
         defaultValue={currentUser.username}
         type='text'
         id='username'
         placeholder='Username'
         className='bg-slate-100 rounded-lg p-3'
-        onChange={handleChange}
-      />
-      <input 
-        defaultValue={currentUser.email}
-        type='email'
-        id='email'
-        placeholder='Email'
-        className='bg-slate-100 rounded-lg p-3'
-        onChange={handleChange}
-      />
-      <input
-        type='password'
-        id='password'
-        placeholder='Password'
-        className='bg-slate-100 rounded-lg p-3'
-        onChange={handleChange}
-      />
+        // onChange={handleChange}
+        />
+        <input 
+          defaultValue={currentUser.email}
+          type='email'
+          id='email'
+          placeholder='Email'
+          className='bg-slate-100 rounded-lg p-3'
+          // onChange={handleChange}
+        />
+        <input
+          type='password'
+          id='password'
+          placeholder='Password'
+          className='bg-slate-100 rounded-lg p-3'
+          // onChange={handleChange}
+        />
 
-      <button 
-        className='bg-slate-700
-       text-white
-       rounded-lg p-3
-       uppercase
-       hover:opacity-95
-       disabled: opacity-80'
-      >
-        Update
-      </button>
+        <button 
+          className='bg-slate-700
+        text-white
+        rounded-lg p-3
+        uppercase
+        hover:opacity-95
+        disabled: opacity-80'
+        >
+          Update
+        </button>
+      </form>
+      <div className='flex justify-between mt-3'>
+        <span className='text-red-600 cursor-pointer'>Delete Account</span>
+        <span className='text-red-600 cursor-pointer'>Sign Out</span>
+      </div>
+      
     </div>
   )
 }
