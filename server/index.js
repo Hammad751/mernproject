fa364c72e3d09045ff70b94460253b648f7aaed9
virtuByVirtual <hammadvtech@gamil.com>
2023-12-07 11:06:25 +0500

    add pre production  changes

--- server/index.js
+++ server/index.js
@@ -4,9 +4,22 @@ import dotenv from  'dotenv'
 import userRoute from './routes/user.route.js'
 import authRoute from './routes/auth.route.js'
 import cookieParser from "cookie-parser";
+import path from "path"
 
-dotenv.config() 
+dotenv.config()
+
+const __dirname = path.resolve(); // this will get the dynamic directory name 
 const app = express();
+
+// in vite we create the build inside "dist" directory
+app.use(express.static(path.join(__dirname, "/client/dist")))
+
+// after creating the static path server send the response to the client
+// at any point from client side, server send send the file 
+app.get("*", (req, res) =>{
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 app.use(express.json());
 app.use(cookieParser())
 const port = 8000;
